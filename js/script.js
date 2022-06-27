2c9b7b590ab54c88945ff866673242b0e6ffff3a
Guido <46787485+guidoFortunato@users.noreply.github.com>
2022-06-27 17:46:19 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -3,26 +3,28 @@ const toggleIcon = document.querySelector("#toggle-icon");
 const toggleText = document.querySelector("#toggle-text");
 const toggleColors = document.querySelector("#toggle-colors");
 const rootStyles = document.documentElement.style; //aqui tenemos todas las variables de nuestro archivo css
-const textsToChange = document.querySelectorAll("[data-section]")
+const textsToChange = document.querySelectorAll("[data-section]");
 
 const flagsElement = document.getElementById("flags");
+
+const initialValuesColors =
+  JSON.parse(localStorage.getItem("colors")) || "#3182ED";
+rootStyles.setProperty("--primary-color", initialValuesColors);
 
 const changeLanguage = async (language) => {
   try {
     const res = await fetch(`./languages/${language}.json`);
     const data = await res.json();
     for (const text of textsToChange) {
-        const section = text.dataset.section
-        const value = text.dataset.value
-        console.log(data[section][value])
-        text.innerHTML = data[section][value]
-        text.innerHTML = data[section][value]
+      const section = text.dataset.section;
+      const value = text.dataset.value;
+      text.innerHTML = data[section][value];
+      text.innerHTML = data[section][value];
     }
   } catch (error) {
     console.log(error);
   }
 };
-
 
 flagsElement.addEventListener("click", (e) => {
   changeLanguage(e.target.parentElement.dataset.language);
@@ -30,18 +32,29 @@ flagsElement.addEventListener("click", (e) => {
 
 toggleTheme.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  if (toggleIcon.src.includes("moon.svg")) {
-    toggleIcon.src = "assets/icons/sun.svg";
-    toggleText.textContent = "Light Mode";
-  } else {
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark-mode", "true");
     toggleIcon.src = "assets/icons/moon.svg";
-    toggleText.textContent = "Dark Mode";
+  } else {
+    localStorage.setItem("dark-mode", "false");
+    toggleIcon.src = "assets/icons/sun.svg";
   }
 });
 
+if (localStorage.getItem("dark-mode") === "true") {
+  document.body.classList.add("dark");
+  toggleIcon.src = "assets/icons/moon.svg";
+} else {
+  document.body.classList.remove("dark");
+  toggleIcon.src = "assets/icons/sun.svg";
+}
+
 toggleColors.addEventListener("click", (e) => {
-  console.log(e.target.dataset.color);
   if (e.target.dataset.color) {
-    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+    localStorage.setItem("colors", JSON.stringify(e.target.dataset.color));
+    rootStyles.setProperty(
+      "--primary-color",
+      JSON.parse(localStorage.getItem("colors"))
+    );
   }
 });
